eed014b3cf05e289b362471171e55475e2958f94
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-05-10 14:41:56 +0100

    set the active link

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,17 +1,32 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import classes from "./MainNavigation.module.css";
 
 function MainNavigation() {
+  const router = useRouter();
+
   return (
     <header className={classes.header}>
       <div className={classes.logo}>React Meetups</div>
       <nav>
         <ul>
           <li>
-            <Link href="/">All Meetups</Link>
+            <Link href="/">
+              <a className={router.pathname == "/" ? classes.active : ""}>
+                All Meetups
+              </a>
+            </Link>
           </li>
           <li>
-            <Link href="/new-meetup">Add New Meetup</Link>
+            <Link href="/new-meetup">
+              <a
+                className={
+                  router.pathname == "/new-meetup" ? classes.active : ""
+                }
+              >
+                Add New Meetup
+              </a>
+            </Link>
           </li>
         </ul>
       </nav>

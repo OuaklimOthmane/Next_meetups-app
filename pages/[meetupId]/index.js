e19fb5c7d086dd77eae70bfc6c8dd3fe182d39a1
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-05-10 17:10:09 +0100

    implementing meetup details page

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,7 +1,16 @@
-import React from "react";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-const MeetupDetail = () => {
-  return <div>Meetup Detail page</div>;
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image={
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+      }
+      title={"the first meetup"}
+      adress={"Some address 10, 12345 Some City"}
+      decription={"this is the first meetup"}
+    />
+  );
 };
 
-export default MeetupDetail;
+export default MeetupDetails;

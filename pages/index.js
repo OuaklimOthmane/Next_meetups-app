20f4b57eabd2fd1a2a84940323508e52e3e52c45
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-05-10 14:06:22 +0100

    Outputting a list of some dummy meetups

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,26 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import MeetupList from "../components/meetups/MeetupList";
 
-export default function Home() {
-  return <div>All meetups page</div>;
-}
+const MEETUP = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address 5, 12345 Some City",
+    description: "This is a first meetup!",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address 10, 12345 Some City",
+    description: "This is a second meetup!",
+  },
+];
+
+const HomePage = () => {
+  return <MeetupList meetups={MEETUP} />;
+};
+
+export default HomePage;

d766af3cdb2eef4a7e705d36b6396494400714d2
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-05-15 00:44:17 +0100

    Add new meetup head metadata

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,4 +1,6 @@
 import { useRouter } from "next/router";
+import Head from "next/head";
+import { Fragment } from "react";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
@@ -19,7 +21,18 @@ const NewMeetupPage = () => {
     router.push("/");
   }
 
-  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>Add a New Meetup</title>
+        <meta
+          name="description"
+          content="Add your own meetups and create amazing networking opportunities."
+        />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </Fragment>
+  );
 };
 
 export default NewMeetupPage;
